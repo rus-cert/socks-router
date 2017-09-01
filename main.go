8ab011df21f6ff8bbe8069258a83ccb47bbe3f38
Stefan Bühler <buehler@cert.uni-stuttgart.de>
2017-09-01 14:33:06 +0200

    support multiple listen addresses

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"net"
+	"strings"
+	"sync"
 
 	homedir "github.com/mitchellh/go-homedir"
 
@@ -10,15 +12,36 @@ import (
 	"github.com/rus-cert/socks-router/routing"
 )
 
+type stringList struct {
+	list     []string
+	defValue []string
+}
+
+func (i *stringList) String() string {
+	return strings.Join(i.Get(), ",")
+}
+
+func (i *stringList) Set(value string) error {
+	i.list = append(i.list, value)
+	return nil
+}
+
+func (i *stringList) Get() []string {
+	if 0 == len(i.list) {
+		return i.defValue
+	}
+	return i.list
+}
+
 var configFile string
-var listenAddr string
+var listenAddrsVar = stringList{nil, []string{"127.0.0.1:8000", "[::1]:8000"}}
 var debugFlag bool
 
 func init() {
 	defConfig, _ := homedir.Expand("~/.socks-routes")
 	flag.BoolVar(&debugFlag, "debug", false, "Enable debug logging")
 	flag.StringVar(&configFile, "config", defConfig, "Path to configfile")
-	flag.StringVar(&listenAddr, "listen", "127.0.0.1:8000", "TCP Address to bind proxy to")
+	flag.Var(&listenAddrsVar, "listen", "TCP Address to bind proxy to; can be passed multiple times")
 }
 
 func main() {
@@ -26,6 +49,7 @@ func main() {
 	if debugFlag {
 		log.EnableDebug()
 	}
+	listenAddrs := listenAddrsVar.Get()
 
 	if routingMap, err := routing.ReadMapFile(configFile); nil != err {
 		log.Error.Fatalf("Couldn't read config file: %v", err)
@@ -46,17 +70,27 @@ func main() {
 			pm.Handlers = append(pm.Handlers, httpHandler)
 		}
 
-		var listener *net.TCPListener
-		log.Info.Printf("socks router listening on %v", listenAddr)
-		if l, err := net.Listen("tcp", listenAddr); nil != err {
-			log.Error.Fatalf("listen failed: %v", err)
-		} else if l, ok := l.(*net.TCPListener); !ok {
-			log.Error.Fatal("listen failed: not TCP")
-		} else {
-			listener = l
-		}
-		defer listener.Close()
+		var wg sync.WaitGroup
 
-		log.Error.Fatal(pm.ListenTCP(listener))
+		for _, addr := range listenAddrs {
+			var listener *net.TCPListener
+			log.Info.Printf("socks router listening on %v", addr)
+			if l, err := net.Listen("tcp", addr); nil != err {
+				log.Error.Fatalf("listen failed: %v", err)
+			} else if l, ok := l.(*net.TCPListener); !ok {
+				log.Error.Fatal("listen failed: not TCP")
+			} else {
+				listener = l
+			}
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				defer listener.Close()
+
+				log.Error.Fatal(pm.ListenTCP(listener))
+			}()
+		}
+		wg.Wait()
 	}
 }
